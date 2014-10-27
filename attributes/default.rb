0faf9117a5c8bf204629a19466d41d2c17c06b8d
Kirill Pavlov <kirill.pavlov@phystech.edu>
2014-10-27 11:53:30 +0800

    update city database name. Use default name.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,3 @@
 default[:geoip][:city_download_link] = "http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz"
 default[:geoip][:directory] = "/usr/share/GeoIP/"
-default[:geoip][:city_db_file] = "GeoIPCity.dat"
+default[:geoip][:city_db_file] = "GeoLiteCity.dat"
